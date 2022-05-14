5b0ccc82c1739bcc5de82f1ea3cf20b32d6617ea
MAPENGMASK <3406157696@qq.com>
2022-05-14 21:08:14 +0800

    format: auto format by IDEA

--- app/src/test/java/todo/java/tdd/exercise/AppTest.java
+++ app/src/test/java/todo/java/tdd/exercise/AppTest.java
@@ -1,10 +1,12 @@
 package todo.java.tdd.exercise;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
