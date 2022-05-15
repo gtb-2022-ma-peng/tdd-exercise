c34833843e420c284f4ffeb52bc6d53b33c0d633
MAPENGMASK <3406157696@qq.com>
2022-05-15 15:31:06 +0800

    chore: delete default generated by gradle

--- app/src/main/java/todo/java/tdd/exercise/App.java
+++ app/src/main/java/todo/java/tdd/exercise/App.java
@@ -1,11 +1,11 @@
 package todo.java.tdd.exercise;
 
+/**
+ * @author Ma Peng
+ */
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
     }
 }
