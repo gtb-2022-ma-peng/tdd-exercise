0648d881546d2dc0746b9869b8aedca017d53604
MAPENGMASK <3406157696@qq.com>
2022-05-16 17:15:39 +0800

    feat: parse `completed` field from task data

---
+++ app/src/test/java/todo/java/tdd/exercise/TaskFactoryTest.java
@@ -0,0 +1,18 @@
+package todo.java.tdd.exercise;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Ma Peng
+ * @date 2022/5/16.
+ */
+class TaskFactoryTest {
+    @Test
+    void should_parse_completed_property_for_task(){
+        boolean isCompleted = TaskFactory.createTask(1, "+ task 01").isCompleted();
+        Assertions.assertFalse(isCompleted);
+    }
+}
