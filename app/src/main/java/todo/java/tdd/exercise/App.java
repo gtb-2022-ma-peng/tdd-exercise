ffe38040e515bca3bea2d414d2e50e0bd29e000c
MAPENGMASK <3406157696@qq.com>
2022-05-20 09:27:54 +0800

    refactor: extract addCommand

--- app/src/main/java/todo/java/tdd/exercise/App.java
+++ app/src/main/java/todo/java/tdd/exercise/App.java
@@ -1,9 +1,5 @@
 package todo.java.tdd.exercise;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 /**
@@ -11,24 +7,15 @@ import java.util.List;
  */
 public class App {
 
-    private final ListCommand listCommand = new ListCommand();
-
     public static void main(String[] args) {
         new App().run().forEach(System.out::println);
     }
 
     public List<String> run(String...args) {
         if (args.length > 0 && args[0].equals("add")) {
-            try (BufferedWriter bw = Files.newBufferedWriter(Constants.TASK_FILE_PATH, StandardOpenOption.APPEND)) {
-                String taskName = args[1];
-                bw.write("+ " + taskName);
-                bw.newLine();
-            } catch (IOException e) {
-                throw new RuntimeException();
-            }
-            return List.of();
+            return new AddCommand(args).execute();
         }
-        return listCommand.run();
+        return new ListCommand().run();
     }
 
 }
--- app/src/main/java/todo/java/tdd/exercise/TaskRepository.java
+++ app/src/main/java/todo/java/tdd/exercise/TaskRepository.java
@@ -1,13 +1,14 @@
 package todo.java.tdd.exercise;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskRepository {
-
     List<Task> loadTasks() {
         List<String> lines = readTaskLines();
         List<Task> tasks = new ArrayList<>();
@@ -22,6 +23,21 @@ public class TaskRepository {
             return Files.readAllLines(Constants.TASK_FILE_PATH, StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new TodoCannotReadFileException();
+        }
+    }
+
+    List<String> create(Task task) {
+        writeTask(task);
+        return List.of();
+    }
+
+    private void writeTask(Task task) {
+        String name = task.getName();
+        try (BufferedWriter bw = Files.newBufferedWriter(Constants.TASK_FILE_PATH, StandardOpenOption.APPEND)) {
+            bw.write("+ " + name);
+            bw.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
     }
 }
