cc21dbc585e318d98d109d632fce8d30cc3ec44f
MAPENGMASK <3406157696@qq.com>
2022-05-20 08:51:12 +0800

    test: make test can repeatable

--- app/src/test/java/todo/java/tdd/exercise/AppTest.java
+++ app/src/test/java/todo/java/tdd/exercise/AppTest.java
@@ -1,33 +1,67 @@
 package todo.java.tdd.exercise;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 class AppTest {
-    @Test
-    void should_list_all_existing_tasks() {
-        List<String> result = new App().run();
-        Assertions.assertEquals(List.of(
-                "# To be done",
-                "1 task 01",
-                "2 task 02",
-                "# Completed",
-                "3 task 03",
-                "4 task 04"), result);
+
+    @BeforeEach
+    void setUp() {
+        writeDataFile(List.of(
+                "+ task 01",
+                "+ task 02",
+                "x task 03",
+                "x task 04"));
     }
-    @Test
-    void should_add_task_with_single_world_as_name() {
-        new App().run("add", "foobar");
-        List<String> result = new App().run();
-        Assertions.assertEquals(List.of(
-                "# To be done",
-                "1 task 01",
-                "2 task 02",
-                "5 foobar",
-                "# Completed",
-                "3 task 03",
-                "4 task 04"), result);
+
+    @Nested
+    class TestList {
+        @Test
+        void should_list_all_existing_tasks() {
+            List<String> result = new App().run();
+            Assertions.assertEquals(List.of(
+                    "# To be done",
+                    "1 task 01",
+                    "2 task 02",
+                    "# Completed",
+                    "3 task 03",
+                    "4 task 04"), result);
+        }
+    }
+
+
+    @Nested
+    class TestAdd {
+        @Test
+        void should_add_task_with_single_world_as_name() {
+            new App().run("add", "foobar");
+            List<String> result = new App().run();
+            Assertions.assertEquals(List.of(
+                    "# To be done",
+                    "1 task 01",
+                    "2 task 02",
+                    "5 foobar",
+                    "# Completed",
+                    "3 task 03",
+                    "4 task 04"), result);
+        }
+    }
+
+    private void writeDataFile(List<String> lines) {
+        try (BufferedWriter bw = Files.newBufferedWriter(Constants.TASK_FILE_PATH)) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
