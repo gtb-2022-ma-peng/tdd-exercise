ab2669520cdceb0985db03c8bd306b495cb36920
MAPENGMASK <3406157696@qq.com>
2022-05-21 09:46:26 +0800

    refactor: extract MyTaskRespository in test

--- app/src/test/java/todo/java/tdd/exercise/AddCommandTest.java
+++ app/src/test/java/todo/java/tdd/exercise/AddCommandTest.java
@@ -11,27 +11,43 @@ import java.util.List;
  * @date 2022/5/21.
  */
 class AddCommandTest {
+
+    private MyTaskRepository taskRepository;
+
+    @BeforeEach
+    void setUp() {
+        taskRepository = new MyTaskRepository();
+    }
+
     @Test
     void should_compose_task_name_using_multiple_args() {
-        final var taskRepository = new TaskRepository() {
-
-            private Task task;
-
-            @Override
-            List<String> create(Task task) {
-                this.task = task;
-                return List.of();
-            }
-
-            public String getName() {
-                return this.task.getName();
-            }
-        };
         AddCommand addCommand = new AddCommand(taskRepository, "add", "foobar", "fizz");
 
         addCommand.execute();
 
-        String taskName = taskRepository.getName();
-        Assertions.assertEquals("foobar fizz", taskName);
+        Assertions.assertEquals("foobar fizz", taskRepository.getName());
+    }
+    @Test
+    void should_use_empty_name_when_no_args_provided() {
+        AddCommand addCommand = new AddCommand(taskRepository, "add");
+
+        addCommand.execute();
+
+        Assertions.assertEquals("", taskRepository.getName());
+    }
+
+    private static class MyTaskRepository extends TaskRepository {
+
+        private Task task;
+
+        @Override
+        List<String> create(Task task) {
+            this.task = task;
+            return List.of();
+        }
+
+        public String getName() {
+            return this.task.getName();
+        }
     }
 }
